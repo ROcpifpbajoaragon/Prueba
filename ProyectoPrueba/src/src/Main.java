94132ee72df914e8e64e8ec6d3f17b4b5e66f259
ROcpifpbajoaragon <rolles@cpifpbajoaragon.com>
2024-01-30 10:49:36 +0100

    #2
    Prueba creada junto con Andres

--- ProyectoPrueba/src/src/Main.java
+++ ProyectoPrueba/src/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Esto es una prueba");
+		System.out.println("Esto es una prueba");
+		System.out.println("Esto es una prueba");
+		System.out.println("Esto es una prueba");
+		System.out.println("Esto es una prueba");
+		System.out.println("Esto es una prueba");
+
 
 	}
 
